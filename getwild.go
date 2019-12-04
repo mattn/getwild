8033d113ca76ada7c4bdd2e46da8b026d09b7e8c
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-12-04 12:53:25 +0900

    Expand args

--- getwild.go
+++ getwild.go
@@ -6,12 +6,18 @@ import (
 )
 
 func init() {
-	args := os.Args
-	for i, arg := range args {
+	os.Args = getwild(os.Args)
+}
+
+func getwild(args []string) []string {
+	result := make([]string, len(args))
+	for _, arg := range args {
 		match, err := filepath.Glob(arg)
 		if err == nil && len(match) > 0 {
-			args[i] = match[0]
+			result = append(result, match...)
+		} else {
+			result = append(result, arg)
 		}
 	}
-	os.Args = args
+	return result
 }
