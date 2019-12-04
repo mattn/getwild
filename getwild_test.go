f7060e0c7775249c13bccf77752d6cc5c5537e24
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-12-04 12:57:36 +0900

    Add test

---
+++ getwild_test.go
@@ -0,0 +1,25 @@
+package getwild
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSimple(t *testing.T) {
+	tests := []struct {
+		input []string
+		want  []string
+	}{
+		{
+			input: []string{"cmd", "-z", "*.go", "-y"},
+			want:  []string{"cmd", "-z", "getwild.go", "getwild_test.go", "-y"},
+		},
+	}
+
+	for _, test := range tests {
+		got := getwild(test.input)
+		if !reflect.DeepEqual(got, test.want) {
+			t.Fatalf("want %v, but %v:", test.want, got)
+		}
+	}
+}
